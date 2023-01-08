c6b7c4c56f33edc8774511752d40e956a3e11a6d
Alexander <ggiba34@gmail.com>
2023-01-08 12:59:31 +0400

    Решил 3 задачу

---
+++ Home_Work3/Zadacha3.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Введите чиcло:  ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 2 == 0)
+{
+  Console.WriteLine("Да");
+
+}
+else
+{
+  Console.WriteLine("Нет");
+}
