40358023f3e440ede2b30c02574c1378fe93737c
Alexander <ggiba34@gmail.com>
2023-01-28 14:56:26 +0400

    Seminar 9

---
+++ Seminar9/HW1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задайте значение N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии
+
+void PrintInterval(int m, int n)
+
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Console.Write($"{i} ");
+			}
+		}
+		PrintInterval(1, 22);
+		
+		Console.WriteLine();
